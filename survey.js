b8b695052ab07ce160c3640a47c8f4e5810c8280
Polina Skrobot <chi.polly.no@gmail.com>
2021-08-25 01:03:50 +0000

    Survey modified

--- survey.js
+++ survey.js
@@ -6,12 +6,12 @@ const rl = readline.createInterface({
 });
 
 rl.question('What is your name? ', (name) => {
-  rl.question("What is your favourite music artist? ", (music)=> {
-    rl.question("What are you interested in? ", (interest)=> {
+  rl.question("What's an activity you like doing? ", (interest)=> {
+    rl.question("What do you listen to while doing that? ", (music)=> {
       rl.question("What is your favourite dish? ", (dish)=> {
         rl.question("What is your lovest meal type (e.g. breakfast, lunch etc.)? ", (meal)=> {
           rl.question("What is your lovest sport? ", (sport)=> {
-            rl.question("What is your superpower? ", (supPow)=> {
+            rl.question("What is your superpower? In a few words, tell us what you are amazing at! ", (supPow)=> {
               console.log(`${name} loves listening to ${music} while ${interest}, devouring ${dish} for
                ${meal}, prefers ${sport} over any other sport, and is amazing at ${supPow}.`);
               rl.close();
